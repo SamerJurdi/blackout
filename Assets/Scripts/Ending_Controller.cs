da45af70670b2d4b946442bd2f5279e8bf37668e
SJ <86448785+SamerJurdi@users.noreply.github.com>
2024-10-27 15:24:11 +0300

    added ending component and resolved functionality

---
+++ Assets/Scripts/Ending_Controller.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ending_Controller : MonoBehaviour
+{
+    [SerializeField] string _victoryMessage = "You escaped!"; 
+
+    private GameObject _player;
+    private Level_Controller _levelController;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+        _levelController = GameObject.FindGameObjectWithTag("Level").GetComponent<Level_Controller>();
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == _player)
+        {
+            _levelController.endGame(_victoryMessage);
+            Destroy(other.gameObject);
+        }
+    }
+}
